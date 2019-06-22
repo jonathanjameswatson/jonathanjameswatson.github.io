874771c65239a7300c218d55e37f541f4343a280
Jonathan Watson <jonathanw12@outlook.com>
2019-06-22 14:25:08 +0100

    Added website

---
+++ README.md
@@ -0,0 +1,22 @@
+# jonathanjameswatson.github.io
+
+> A website for my projects
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,173 @@
+<template>
+  <section>
+    <section id="intro" class="hero is-fullheight">
+      <div id="p5-canvas" class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column has-text-centered is-narrow">
+              <div class="box">
+                <h1 class="title is-1">jonathan<wbr />james<wbr />watson</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="columns is-multiline">
+        <div class="column has-text-centered is-one-third">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img
+                  src="~assets/images/exhaustivesearchcalculator.png"
+                  alt="Exhaustive search calculator"
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <h1 class="subtitle is-4">Exhaustive search calculator</h1>
+            </div>
+            <div class="card-footer">
+              <a class="card-footer-item" href="exhaustive-search-calculator">
+                Website
+              </a>
+              <a
+                class="card-footer-item"
+                href="https://github.com/jonathanjameswatson/exhaustive-search-calculator"
+              >
+                Github</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="column has-text-centered is-one-third">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img
+                  src="~assets/images/numericallysolvingequations.png"
+                  alt="Numerically solving equations"
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <h1 class="subtitle is-4">Numerically solving equations</h1>
+            </div>
+            <div class="card-footer">
+              <a class="card-footer-item" href="numerically-solving-equations"
+                >Website</a
+              >
+              <a
+                class="card-footer-item"
+                href="https://github.com/jonathanjameswatson/numerically-solving-equations"
+                >Github</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="column has-text-centered is-one-third">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="~assets/images/truthtrick.png" alt="Truth Trick" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <h1 class="subtitle is-4">Truth Trick</h1>
+            </div>
+            <div class="card-footer">
+              <a class="card-footer-item" href="truth-trick">Website</a>
+              <a
+                class="card-footer-item"
+                href="https://github.com/jonathanjameswatson/truth-trick"
+                >Github</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="column has-text-centered is-one-third">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="~assets/images/mathsrace.png" alt="Maths Race" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <h1 class="subtitle is-4">Maths Race</h1>
+            </div>
+            <div class="card-footer">
+              <a
+                class="card-footer-item"
+                href="https://github.com/jonathanjameswatson/maths-race/"
+                >Github</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="column has-text-centered is-one-third">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="~assets/images/bhive.png" alt="bHive" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <h1 class="subtitle is-4">bHive</h1>
+            </div>
+            <div class="card-footer">
+              <a
+                class="card-footer-item"
+                href="https://github.com/CodeGuild-co/bHive"
+                >Github</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="column has-text-centered is-one-third">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="~assets/images/mygithub.png" alt="My GitHub" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <h1 class="subtitle is-4">My GitHub</h1>
+            </div>
+            <div class="card-footer">
+              <a
+                class="card-footer-item"
+                href="https://github.com/jonathanjameswatson/"
+                >Github</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+let background;
+if (process.browser) {
+  background = require('@/js/background.js');
+}
+export default {
+  data() {
+    return {
+      message: ''
+    };
+  },
+  mounted() {
+    const P5 = require('p5');
+    (() => new P5(background.main))();
+  }
+};
+</script>
